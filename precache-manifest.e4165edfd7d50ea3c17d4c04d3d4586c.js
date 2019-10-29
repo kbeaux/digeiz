6ff62b707969163f45e7f633fc96491668eb774e
kbeaux <beauxkevin@gmail.com>
2019-10-29 09:35:57 +0100

    Updates

---
+++ precache-manifest.e4165edfd7d50ea3c17d4c04d3d4586c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a752da42e7ba64b3df0dcc8593f93a",
+    "url": "/digeiz/index.html"
+  },
+  {
+    "revision": "69839d9d9efe5e4d822d",
+    "url": "/digeiz/static/css/main.5d7239b8.chunk.css"
+  },
+  {
+    "revision": "311b65c805c64ae1fe00",
+    "url": "/digeiz/static/js/2.234f8d71.chunk.js"
+  },
+  {
+    "revision": "69839d9d9efe5e4d822d",
+    "url": "/digeiz/static/js/main.3827e855.chunk.js"
+  },
+  {
+    "revision": "3c8a4e9ee1956dafc2e1",
+    "url": "/digeiz/static/js/runtime-main.61a4236a.js"
+  }
+]);
